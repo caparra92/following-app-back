803c187574294757ee2ab99f839c331570463e7b
Camilo Parra <caparra92@gmail.com>
2023-04-09 11:35:57 -0500

    Verifying db connectiion variable

--- src/config/config.ts
+++ src/config/config.ts
@@ -45,12 +45,12 @@ if(node_env !== 'development'){
 }
 
 interface ConfigurationObject {
-    dev: any,
+    development: any,
     test: any,
-    prod: any
+    production: any
 }
 
-const config: any = {
+const config: ConfigurationObject = {
     development: {
         db: {
             username,
@@ -73,6 +73,6 @@ console.log("CONFIG PROD username is:::"+config.production.db.username)
 console.log("CONFIG PROD password is:::"+config.production.db.password)
 console.log("CONFIG PROD database is:::"+config.production.db.database)
 console.log("CONFIG PROD host is:::"+config.production.db.host)
-console.log("Config env is:::"+config[node_env])
+console.log("Config env is:::"+config[node_env as keyof ConfigurationObject])
 
-export const env =  config[node_env];
+export const env =  config[node_env as keyof ConfigurationObject];
